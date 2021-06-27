6d5d51abfe72b7d2eed0c967382fc3e7d423a025
Eric Hongmoungkhoune <ehmk90@gmail.com>
2021-06-26 22:42:25 -0400

    Separated the code meant for the home tab into a different module.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,6 @@
 import "./styles/style.css";
-import cozyRestaurant from "./images/cozy-restaurant-1.jpg";
+import { home } from "./home.js";
 
 const content = document.querySelector("#content");
+content.appendChild(home);
 
-const headline = document.createElement("h1");
-headline.textContent = "RESTAURANT";
-content.appendChild(headline);
-
-const restaurantDescription = document.createElement("p");
-restaurantDescription.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-content.appendChild(restaurantDescription);
-
-const restaurantImage = new Image();
-restaurantImage.src = cozyRestaurant;
-restaurantImage.classList.add("gallery-image");
-content.appendChild(restaurantImage);
-
-console.log("HELLO WORLD!!!");
